86fb3cf770759013e0e3dd1f71de2b6c33e91269
MatthewFavela <mfavela3508@myhta.org>
2018-11-01 15:24:11 -1000

    update

---
+++ app.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiaHRhZGV2cyIsImEiOiJjam1sY3dhZzYwNnZ0M3JsYnhncHRxaGtoIn0.JIObY2NWGtwzQnHCh2TIjw';
+const map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/htadevs/cjnqk2ozp14aa2sp0dj1zo5k8',
+center: [-157.719,21.276],
+zoom: 14.6
+});
